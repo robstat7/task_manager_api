1a654801b4f0206373b0cfb58a93da7272b6b3ac
Dileep Sankhla <dileepsankhla.ds@gmail.com>
2026-05-01 13:33:44 +0530

    Add functions to get db and close db per request

---
+++ task_manager/db.py
@@ -0,0 +1,29 @@
+import sqlite3
+from datetime import datetime
+
+import click
+from flask import current_app, g
+
+# get_db
+#
+# This function will be called when the application has been created
+# and is handling a request.
+# Note that g is a special object that is unique for each request.
+# It is used to store data that might be accessed by multiple
+# functions during the request.
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
